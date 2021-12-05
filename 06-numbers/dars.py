2fd520426d6bf25297943721a59198ad28a002a8
ilosrim <ilosrim@yahoo.com>
2021-12-05 20:28:23 +0500

    edit numbers

---
+++ 06-numbers/dars.py
@@ -0,0 +1,9 @@
+# son = 10
+
+# son = 7_567_896_324
+# print(f"Aholi soni: {son}")
+
+a = 22
+b = 22.2
+x, y, z = 255, -155, 25
+print(x, y, z, type(b))
--- 09-xatolar-bli/app.js
+++
@@ -1,2 +0,0 @@
-let age = 7;
-console.log('Tommy is ${age} years old');
