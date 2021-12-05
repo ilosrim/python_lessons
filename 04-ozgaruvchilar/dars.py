9a9a19725bee2698aa84c897440879f70247d311
ilosrim <ilosrim@yahoo.com>
2021-12-05 20:06:40 +0500

    edit

--- 04-ozgaruvchilar/dars.py
+++ 04-ozgaruvchilar/dars.py
@@ -24,4 +24,4 @@
 radius = 5
 pi = 3.14159
 aylana_yuzi = pi * radius**2
-print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
+print(f"Radiusi {radius} ga teng aylananing yuzi = {aylana_yuzi} ga teng")
